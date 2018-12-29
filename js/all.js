9b55ce815f3a86dbc916e13efa2791b6a3d5310e
yinyaoqing <42916369+yinyaoqing@users.noreply.github.com>
2018-12-29 14:36:51 +0800

    update

---
+++ js/all.js
@@ -0,0 +1,98 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        newTodo: '',
+        todos: [{
+            id: 1,
+            title: '快來建立自己的代辦清單吧！',
+            completed: false,
+        }],
+        visibility: 'all',
+        cacheTodo: {},
+        cacheTitle: '',
+        actLength: '',
+    },
+    methods: {
+        addTodo: function () {
+            var value = this.newTodo.trim();
+            var timestamp = Math.floor(Date.now());
+            if (!value) {
+                alert('請輸入代辦事項！')
+                return;
+            }
+            this.todos.push({
+                id: timestamp,
+                title: value,
+                completed: false,
+            });
+            localStorage.setItem('listData', JSON.stringify(this.todos));
+            this.newTodo = '';
+        },
+        removeTodo: function (todo) {
+            var newIndex = '';
+            this.todos.forEach(function (item, key) {
+                if (todo.id == item.id) {
+                    newIndex = key
+                }
+            })
+            this.todos.splice(newIndex, 1);
+            localStorage.setItem('listData', JSON.stringify(this.todos));
+        },
+        editTodo: function (item) {
+            this.cacheTodo = item;
+            this.cacheTitle = item.title;
+        },
+        cancelEdit: function () {
+            this.cacheTodo = {};
+        },
+        doneEdit: function (item) {
+            item.title = this.cacheTitle;
+            this.cacheTitle = '';
+            this.cacheTodo = {};
+        },
+        removeAll: function () {
+            this.todos = [];
+            localStorage.setItem('listData', JSON.stringify(this.todos));
+        },
+    },
+    computed: {
+        filteredTodo: function () {
+            this.todos = JSON.parse(localStorage.getItem('listData')) || [];
+            if (this.visibility == 'all') {
+                return this.todos;
+            } else if (this.visibility == 'act') {
+                var actTodo = [];
+                this.todos.forEach(function (item) {
+                    if (!item.completed) {
+                        actTodo.push(item);
+                    }
+                })
+                return actTodo;
+            }
+            else if (this.visibility == 'done') {
+                var doneTodo = [];
+                this.todos.forEach(function (item) {
+                    if (item.completed) {
+                        doneTodo.push(item);
+                    }
+                })
+                return doneTodo;
+            }
+        },
+        actTodo: function () {
+            var actTodo = [];
+            this.todos.forEach(function (item) {
+                if (!item.completed) {
+                    actTodo.push(item);
+                }
+            })
+            return actTodo.length;
+        }
+    },
+    updated(){
+        localStorage.setItem('listData', JSON.stringify(this.todos));
+    }
+});
+
+  
